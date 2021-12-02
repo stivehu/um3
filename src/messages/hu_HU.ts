28046a5d93f16baa706691719d65128362f50b0a
Gábor István <stive.hu@gmail.com>
2021-12-02 17:37:08 +0100

    fordítások frissítése

--- src/messages/hu_HU.ts
+++ src/messages/hu_HU.ts
@@ -1,215 +1,242 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="hu_HU">
+<!DOCTYPE TS><TS version="2.0" language="hu_HU" sourcelanguage="">
 <context>
     <name>Form</name>
     <message>
         <location filename="../views/settings/settings.ui" line="20"/>
         <source>Form</source>
-        <translation type="unfinished">Rajtszám kiadás</translation>
+        <translation type="obsolete">Rajtszám kiadás</translation>
     </message>
     <message>
         <location filename="../views/settings/settings.ui" line="32"/>
         <source>Server Ip</source>
-        <translation>Szerver ip-je</translation>
+        <translation type="obsolete">Szerver ip-je</translation>
     </message>
     <message>
         <location filename="../views/settings/settings.ui" line="59"/>
         <source>Chipcontroll Interval</source>
-        <translation>Csip kontroll frissítési idő</translation>
+        <translation type="obsolete">Csip kontroll frissítési idő</translation>
     </message>
     <message>
         <location filename="../views/settings/settings.ui" line="86"/>
         <source>Chipcontroll Wait After Read</source>
-        <translation>Csip kontroll megjelenítési idő</translation>
+        <translation type="obsolete">Csip kontroll megjelenítési idő</translation>
     </message>
     <message>
         <location filename="../views/settings/settings.ui" line="113"/>
         <source>Auto Maximize Opening Window</source>
-        <translation>Automatikus teljes képernyő</translation>
+        <translation type="obsolete">Automatikus teljes képernyő</translation>
     </message>
     <message>
         <location filename="../views/settings/settings.ui" line="137"/>
         <source>Auto Resize Window</source>
-        <translation>Automatikus ablak átmértetezés</translation>
+        <translation type="obsolete">Automatikus ablak átmértetezés</translation>
     </message>
     <message>
         <location filename="../views/settings/settings.ui" line="161"/>
         <source>Comm Port</source>
-        <translation>com port</translation>
+        <translation type="obsolete">com port</translation>
     </message>
     <message>
         <location filename="../views/settings/settings.ui" line="212"/>
         <source>save</source>
-        <translation>Mentés</translation>
+        <translation type="obsolete">Mentés</translation>
     </message>
     <message>
-        <location filename="../views/entrypickup/entrypickup.ui" line="17"/>
-        <location filename="../views/localentry/localentry.ui" line="23"/>
+        <location filename="../views/localentry/localentry.py" line="356"/>
         <source>startnum pickup</source>
         <translation>Rajtszám átvétele</translation>
     </message>
     <message>
-        <location filename="../views/entrypickup/entrypickup.ui" line="37"/>
-        <location filename="../views/localentry/localentry.ui" line="431"/>
+        <location filename="../views/localentry/localentry.py" line="369"/>
         <source>Read rfid</source>
         <translation>Rajtszám olvasása</translation>
     </message>
     <message>
-        <location filename="../views/entrypickup/entrypickup.ui" line="55"/>
+        <location filename="../views/entrypickup/entrypickup.py" line="247"/>
         <source>Entry pickup</source>
         <translation>Rajtszám kiadása</translation>
     </message>
     <message>
-        <location filename="../views/chipcontroll/chipcontroll.ui" line="17"/>
+        <location filename="../views/chipcontroll/chipcontroll.py" line="212"/>
         <source>chip controll</source>
         <translation>chip kontrol</translation>
     </message>
     <message>
-        <location filename="../views/chipcontroll/chipcontroll.ui" line="42"/>
-        <location filename="../views/entrypickup/entrypickup.ui" line="120"/>
-        <location filename="../views/localentry/localentry.ui" line="338"/>
+        <location filename="../views/localentry/localentry.py" line="365"/>
         <source>Gender</source>
         <translation>Nem</translation>
     </message>
     <message>
-        <location filename="../views/chipcontroll/chipcontroll.ui" line="60"/>
-        <location filename="../views/entrypickup/entrypickup.ui" line="159"/>
-        <location filename="../views/localentry/localentry.ui" line="54"/>
+        <location filename="../views/localentry/localentry.py" line="357"/>
         <source>Start num</source>
         <translation>Rajtszám</translation>
     </message>
     <message>
-        <location filename="../views/chipcontroll/chipcontroll.ui" line="276"/>
-        <location filename="../views/entrypickup/entrypickup.ui" line="216"/>
-        <location filename="../views/localentry/localentry.ui" line="295"/>
+        <location filename="../views/localentry/localentry.py" line="364"/>
         <source>First name</source>
         <translation>Keresztnév</translation>
     </message>
     <message>
-        <location filename="../views/chipcontroll/chipcontroll.ui" line="216"/>
-        <location filename="../views/entrypickup/entrypickup.ui" line="276"/>
+        <location filename="../views/chipcontroll/chipcontroll.py" line="218"/>
         <source>Picked up</source>
         <translation>Rajtszám állapota</translation>
     </message>
     <message>
-        <location filename="../views/chipcontroll/chipcontroll.ui" line="78"/>
-        <location filename="../views/entrypickup/entrypickup.ui" line="312"/>
-        <location filename="../views/localentry/localentry.ui" line="252"/>
+        <location filename="../views/localentry/localentry.py" line="363"/>
         <source>Lastname</source>
         <translation>Vezetéknév</translation>
     </message>
     <message>
-        <location filename="../views/chipcontroll/chipcontroll.ui" line="135"/>
-        <location filename="../views/entrypickup/entrypickup.ui" line="294"/>
-        <location filename="../views/localentry/localentry.ui" line="97"/>
+        <location filename="../views/localentry/localentry.py" line="358"/>
         <source>distance</source>
         <translation>Táv</translation>
     </message>
     <message>
-        <location filename="../views/entrypickup/entrypickup.ui" line="356"/>
+        <location filename="../views/entrypickup/entrypickup.py" line="256"/>
         <source>Entry pickdown</source>
         <translation>Rajtszám visszavétele</translation>
     </message>
     <message>
-        <location filename="../views/chipcontroll/chipcontroll.ui" line="96"/>
-        <location filename="../views/entrypickup/entrypickup.ui" line="177"/>
-        <location filename="../views/localentry/localentry.ui" line="214"/>
+        <location filename="../views/localentry/localentry.py" line="362"/>
         <source>Age group</source>
         <translation>Korosztály</translation>
     </message>
     <message>
-        <location filename="../views/localentry/localentry.ui" line="129"/>
+        <location filename="../views/localentry/localentry.py" line="359"/>
         <source>Birthday</source>
         <translation>Születésnap</translation>
     </message>
     <message>
-        <location filename="../views/localentry/localentry.ui" line="360"/>
+        <location filename="../views/localentry/localentry.py" line="366"/>
         <source>Man</source>
         <translation>Férfi</translation>
     </message>
     <message>
-        <location filename="../views/localentry/localentry.ui" line="365"/>
+        <location filename="../views/localentry/localentry.py" line="367"/>
         <source>Woman</source>
         <translation>Nő</translation>
     </message>
     <message>
-        <location filename="../views/localentry/localentry.ui" line="388"/>
+        <location filename="../views/localentry/localentry.py" line="368"/>
         <source>Settlement</source>
         <translation>Település</translation>
     </message>
     <message>
-        <location filename="../views/localentry/localentry.ui" line="471"/>
+        <location filename="../views/localentry/localentry.py" line="370"/>
         <source>New entry</source>
         <translation>Törlés</translation>
     </message>
     <message>
-        <location filename="../views/localentry/localentry.ui" line="487"/>
+        <location filename="../views/localentry/localentry.py" line="371"/>
         <source>Save entry</source>
         <translation>Nevező mentése</translation>
     </message>
     <message>
-        <location filename="../views/entrypickup/entrypickup.ui" line="58"/>
+        <location filename="../views/entrypickup/entrypickup.py" line="248"/>
         <source>Ctrl+A</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../views/localentry/localentry.ui" line="167"/>
+        <location filename="../views/localentry/localentry.py" line="360"/>
         <source>&quot; &quot;</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../views/localentry/localentry.ui" line="183"/>
+        <location filename="../views/localentry/localentry.py" line="361"/>
         <source>yyyy-MM-dd</source>
         <translation></translation>
+    </message>
+    <message>
+        <location filename="../controller/LocalentryWindow.py" line="122"/>
+        <source>This field required  </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controller/LocalentryWindow.py" line="130"/>
+        <source>This field must be select </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controller/LocalentryWindow.py" line="137"/>
+        <source>Startnum is required field </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controller/LocalentryWindow.py" line="144"/>
+        <source>Invalid birthday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controller/LocalentryWindow.py" line="159"/>
+        <source>New entry added</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controller/LocalentryWindow.py" line="173"/>
+        <source>Are you sure? This page will be destroyed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../controller/LocalentryWindow.py" line="174"/>
+        <source>Create new entry</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>UserMangerUi</name>
     <message>
-        <location filename="../views/mainwindow/mainwindow.ui" line="15"/>
+        <location filename="../views/mainwindow/mainwindow.py" line="125"/>
         <source>User manager</source>
         <translation>Nevező kezelés</translation>
     </message>
     <message>
-        <location filename="../views/mainwindow/mainwindow.ui" line="34"/>
+        <location filename="../views/mainwindow/mainwindow.py" line="126"/>
         <source>Add local entry</source>
         <translation>Helyszíni nevezés</translation>
     </message>
     <message>
-        <location filename="../views/mainwindow/mainwindow.ui" line="47"/>
+        <location filename="../views/mainwindow/mainwindow.py" line="127"/>
         <source>start num pickup</source>
         <translation>Rajtszám kiadás</translation>
     </message>
     <message>
-        <location filename="../views/mainwindow/mainwindow.ui" line="60"/>
+        <location filename="../views/mainwindow/mainwindow.py" line="128"/>
         <source>Chip controll</source>
         <translation>Csip kontroll</translation>
     </message>
     <message>
-        <location filename="../views/mainwindow/mainwindow.ui" line="73"/>
+        <location filename="../views/mainwindow/mainwindow.py" line="129"/>
         <source>show results</source>
         <translation>Eredmény kijelző</translation>
     </message>
     <message>
-        <location filename="../views/mainwindow/mainwindow.ui" line="86"/>
+        <location filename="../views/mainwindow/mainwindow.py" line="130"/>
         <source>show RFID</source>
         <translation>RFID megjelenítés</translation>
     </message>
     <message>
-        <location filename="../views/mainwindow/mainwindow.ui" line="115"/>
+        <location filename="../views/mainwindow/mainwindow.py" line="131"/>
         <source>Preentrys</source>
         <translation>Előnevezők csipezése</translation>
     </message>
     <message>
-        <location filename="../views/mainwindow/mainwindow.ui" line="128"/>
+        <location filename="../views/mainwindow/mainwindow.py" line="132"/>
         <source>Settings</source>
         <translation>Beállítások</translation>
     </message>
     <message>
-        <location filename="../views/mainwindow/mainwindow.ui" line="137"/>
+        <location filename="../views/mainwindow/mainwindow.py" line="133"/>
         <source>E&amp;xit</source>
         <translation>&amp;Kilépés</translation>
+    </message>
+</context>
+<context>
+    <name>models</name>
+    <message>
+        <location filename="../models/GenderModel.py" line="15"/>
+        <source>Server error</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
