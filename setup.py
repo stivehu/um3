31be26b94e5214c91249695c2b47298b114718cb
Gábor István <stive.hu@gmail.com>
2026-08-17 10:43:00 +0200

    chore: Bővíti a Windows build Qt plugin kezelését   - Felveszi a cx_Freeze Qt plugin include_files logikáját (accessible, iconengines, webview, wayland-*)

--- setup.py
+++ setup.py
@@ -1,11 +1,44 @@
 import sys
 from cx_Freeze import setup, Executable
+
+try:
+    from cx_Freeze.hooks import get_qt_plugins_paths
+except ImportError:
+    get_qt_plugins_paths = None
+
 import os
 
 # Dependencies are automatically detected, but it might need fine tuning.
 # "packages": ["os"] is used as example only
 translate = os.path.join(os.path.dirname(__file__), "src", "messages", "hu_HU.qm")
-build_exe_options = {"excludes": ["tkinter"], "include_files": [translate], "optimize": 2}
+include_files = [translate]
+if get_qt_plugins_paths:
+    # Inclusion of extra plugins (since cx_Freeze 6.8b2)
+    # cx_Freeze imports automatically the following plugins depending of the
+    # use of some modules:
+    # imageformats, platforms, platformthemes, styles - QtGui
+    # mediaservice - QtMultimedia
+    # printsupport - QtPrintSupport
+    for plugin_name in (
+         "accessible",
+         "iconengines",
+         "webview",
+        # "platforminputcontexts",
+        # "xcbglintegrations",
+        # "egldeviceintegrations",
+        "wayland-decoration-client",
+        "wayland-graphics-integration-client",
+        # "wayland-graphics-integration-server",
+        "wayland-shell-integration",
+    ):
+        include_files += get_qt_plugins_paths("PyQt5", plugin_name)
+        print (include_files)
+include_files = [
+    (os.path.join(os.path.dirname(sys.executable), 'Library', 'plugins'), 'plugins')
+]        
+
+
+build_exe_options = {"excludes": ["tkinter"], "include_files": [include_files], "optimize": 2}
 bdist_msi_options = {
     "all_users": True
 }
